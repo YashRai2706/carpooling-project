d22d80af593dc835f9aa9c47aeb76094b714fe31
yashrai123 <42269035+yashrai123@users.noreply.github.com>
2018-12-04 01:23:32 +0530

    Add files via upload

---
+++ src/java/BookNow.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Yash
+ */
+public class BookNow extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.cj.jdbc.Driver");   
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb?useSSL=false","root","yashrai123");
+           
+           PreparedStatement pstmt=conn.prepareStatement("insert into(DriverName,pickup,destination,seatsavail,regisnum) values(?,?,?,?,?)");
+           String s1 = request.getParameter("place1");
+           String s2 = request.getParameter("place2");
+           String s3 = request.getParameter("place3");
+           String s4 = request.getParameter("place4");
+           String s5 = request.getParameter("place5");
+           
+           
+           
+           
+           
+           pstmt.setString(1,s1);
+           pstmt.setString(2,s2);
+           pstmt.setString(3,s3);
+           pstmt.executeUpdate();
+        }
+    catch(Exception e)
+    {
+        System.out.println(e);
+        
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
